53e27e30fa34978e4ee54df76dea061e6bf8468e
Jeremy List <quick.dudley@gmail.com>
2014-05-16 14:48:09 +0800

    Move generator

--- Board.hs
+++ Board.hs
@@ -1,16 +1,33 @@
 module Board (
+  Player(..),
+  Direction(..),
+  Step(..),
+  Board,
+  step,
+  onBoard,
+  rotateBoard,
+  startBoard2,
+  startBoard3
  ) where
 
+import Control.Monad (guard)
+import Data.List (nub)
 import qualified Data.Map as M
 import Data.Int
+import Data.Maybe (catMaybes)
 
 data Player = Red | Green | Blue
-  deriving (Enum,Bounded,Show)
+  deriving (Eq,Enum,Bounded,Show)
 
 data Direction = NE | EE | SE | SW | WW | NW
-  deriving (Enum,Show)
+  deriving (Eq,Enum,Bounded,Show)
+
+data Step = SL | SF | SR deriving (Enum,Show)
 
 type Board = M.Map (Int8,Int8) (Player,Direction)
+
+turn :: Int -> Direction -> Direction
+turn n = toEnum . (`mod` 6) . (+ n) . fromEnum
 
 {-
     (0,1)   (1,1)
@@ -26,18 +43,15 @@ step (x,y) d = case d of
   WW -> (x-1,y)
   NW -> (x,y+1)
 
+opposing :: Direction -> Direction
+opposing = turn 3
+
 {-
-           (0,5)                                     (5,5)
-         (-1,4)                                        (5,4)
-       (-2,3)                                            (5,3)
-     (-3,2)                                                (5,2)
-   (-4,1)  (-3,1) (-2,1) (-1,1) (0,1) (1,1) (2,1) (3,1) (4,1) (5,1)
-(-5,0) (-4,0) (-3,0) (-2,0) (-1,0) (0,0) (1,0) (2,0) (3,0) (4,0) (5,0)
-   (-5,-1)                                                    (4,-1)
-     (-5,-2)                                                (3,-2)
-       (-5,-3)                                            (2,-3)
-         (-5,-4)                                        (1,-4)
-           (-5,-5)                                    (0,-5)
+In the original Vala implementation, I swapped the x and y coordinates due to a
+coding error, and put (0,0) in the Southwest corner. In this implementation
+(0,0) is in the center, and the corners are at these coordinates:
+
+(-5,-5), (0,-5), (5,0), (5,5), (0,5), and (-5,0)
 -}
 
 boardRange :: [(Int8,Int8)]
@@ -59,6 +73,8 @@ onBoard (x,y) =
   where
     isqb n = n >= (-5) && n <= 5
 
+-- Lay out the player's pieces at Blue's position, the functions for the real
+-- starting positions will call this then rotate appropriately.
 startp' :: Player -> Board
 startp' player = M.fromList $ do
   y <- [-5,-4]
@@ -67,9 +83,15 @@ startp' player = M.fromList $ do
     then (player,NE)
     else (player,NW))
 
+{-
+The matrix representing a 1/6 turn in this coordinate system is:
+⎛ 1 1⎞
+⎝-1 0⎠
+Multiplying it by itself gives the matrices represented in 4-tuples below.
+-}
 rotateBoard :: Int -> Board -> Board
 rotateBoard 0 b = b
-rotateBoard n bd = (M.fromList . map rotate1 . M.toList) (bd :: Board) where
+rotateBoard n bd = (M.fromList . map rotate1 . M.toList) bd where
   rotate1 (t,(p,d)) = (rt t,(p,rd d))
   rt (x,y) = (a*x+c*y, b*x+d*y)
   (a,b,c,d) = case n `mod` 6 of
@@ -79,8 +101,32 @@ rotateBoard n bd = (M.fromList . map rotate1 . M.toList) (bd :: Board) where
     3 -> (-1,0,0,-1)
     4 -> (-1,-1,1,0)
     5 -> (0,-1,1,1)
-  rd = toEnum . (`mod` 6) . (+ (6 - n)) . fromEnum
+  rd = turn (6 - n)
 
 startBoard2 = M.union (startp' Blue) (rotateBoard 3 $ startp' Red)
-startBoard3 = foldl1 M.union [startp' Blue, rotateBoard 2 $ startp' Green, rotateBoard 4 $ startp' Red]
+startBoard3 = foldl1 M.union [
+  startp' Blue,
+  rotateBoard 2 $ startp' Green,
+  rotateBoard 4 $ startp' Red
+ ]
+
+genMoves p b = nub $ do
+  b' <- genMoves' p b
+  genMoves' p b'
+ where
+  genMoves' p b = do
+    ((x,y),(p',d)) <- M.toList b
+    guard (p == p')
+    let
+      front = step (x,y) d
+      canForward = onBoard front && case M.lookup front b of
+        Just (_,d') -> opposing d /= d'
+        Nothing -> True
+    catMaybes [
+      Just (M.insert (x,y) (p,turn 5 d) b),
+      if canForward
+        then Just (M.delete (x,y) $ M.insert front (p,d) b)
+        else Nothing,
+      Just (M.insert (x,y) (p,turn 1 d) b)
+     ]
 
