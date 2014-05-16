b7053acd2da5c2eaf630e1f088ac0c9301f70c89
Jeremy List <quick.dudley@gmail.com>
2014-05-16 22:59:12 +0800

    Rewrite backprop algorithm
    Backprop appears to be working now, but I've only tested it on single layer networks.

--- Neural.hs
+++ Neural.hs
@@ -2,6 +2,7 @@ module Neural (
  ) where
 
 import Data.List
+import System.Random
 
 type Layer = [[Double]]
 type NNet = [Layer]
@@ -19,15 +20,29 @@ feedforward = flip (foldl' feedlayer)
 feedlayer i = map (sigmoid . sum . zipWith (*) (1:i))
 
 backprop :: Double -> [Double] -> [Double] -> NNet -> NNet
-backprop rate i e n = fst $ backprop' i e n where
-  backprop' i ef [] = ([],zipWith subtract i ef)
-  backprop' i ef (l:nr) = let
+backprop rate i t n = fst $ backprop' i t n where
+  backprop' i t (l:n) = (nw:r,be) where
     hs = feedlayer i l
-    (nr',ec) = backprop' (hs) ef nr
-    em = zipWith bem l ec
-    bem ws err = zipWith (*) (1:i) $ map (* err) ws
-    nes = map sum em
-    updateNode ws err = zipWith3 (updateWeight err) ws i hs
-    updateWeight err w a h = w + rate * err * h * a
-    in (zipWith updateNode l nes : nr',map sum $ transpose em)
+    (r,e) = case n of
+      [] -> ([], zipWith subtract hs t)
+      x -> backprop' hs t n
+    we = zipWith (\oe w ->
+      map (*oe) w
+     ) e l
+    nw = zipWith3 (\wl dl h -> let sdh = sigmoidDerivative h in
+      zipWith3 (\w d x ->
+        w + rate * d * sdh * x
+       ) wl dl (1:i)
+     ) l we hs
+    be = map sum $ transpose we
+
+randomNNet :: RandomGen g => g -> [Int] -> NNet
+randomNNet _ [_] = []
+randomNNet gen (i:r@(n:_)) = let
+  frl g _ = mapAccumL (\g _ -> let
+    (a,g') = randomR (-0.05,0.05) g
+    in (g',a)) g $ replicate (i+1) ()
+  frg g = mapAccumL frl g $ replicate n ()
+  (gen',l1) = frg gen
+  in l1 : randomNNet gen' r
 
