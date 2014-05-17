ecbdae2f165edd641b8186bd6ae04996f54d8ba3
Jeremy List <quick.dudley@gmail.com>
2014-05-17 23:41:47 +0800

    In test: change output format for use by gnuplot, tweak parameters

--- test.hs
+++ test.hs
@@ -1,18 +1,21 @@
 import Data.List
 import System.Random
+import System.IO
 import Neural
 
 main = do
   g <- getStdGen
-  let un = randomNNet g [2,4,1]
-  uloop un
-  where
+  csv <- openFile "learning.dat" WriteMode
+  let un = randomNNet g [2,16,1]
+  let
    uloop n = do
      let r = concatMap (feedforward n) [[0,0],[1,1],[1,0],[0,1]]
-     print r
+     hPutStrLn csv $ intercalate " " $ map show r
      case map (\n -> (n < 0.1, n > 0.9)) r of
        [(True,_),(True,_),(_,True),(_,True)] -> do
+          hClose csv
           putStrLn "Done"
           print n
-       _ -> uloop $ (backprop 0.03 [0,0] [0] . backprop 0.03 [0,1] [1] . backprop 0.03 [1,1] [0] . backprop 0.03 [1,0] [1]) n
+       _ -> uloop $ (backprop 0.15 [0,0] [0] . backprop 0.15 [0,1] [1] . backprop 0.15 [1,1] [0] . backprop 0.15 [1,0] [1]) n
+  uloop un
 
