55280721cda15c46506bf9de2ed630d53c52f92e
Jeremy List <quick.dudley@gmail.com>
2017-06-12 00:22:25 +1200

    Refactor Neural module to use arrays

--- Neural.hs
+++ Neural.hs
@@ -7,11 +7,27 @@ module Neural (
   randomNNet
  ) where
 
+import Data.Array.Unboxed
 import Data.List
 import System.Random
 
-type Layer = [[Double]]
+newtype Layer = Layer (UArray (Int,Int) Double) deriving (Eq)
 type NNet = [Layer]
+
+instance Read Layer where
+  readsPrec p s = map (\ ~(a,r) -> (ul a, r)) $ readsPrec p s where
+    ul l = let
+      w = (minimum $ map length l) - 1
+      h = length l - 1
+      in Layer $ array ((0,0),(w,h)) $ do
+        (y,r) <- zip [0 .. h] l
+        (x,v) <- zip [0 .. w] r
+        return ((x,y),v)
+
+instance Show Layer where
+  showsPrec p (Layer l) = let
+    ((x0,y0),(xn,yn)) = bounds l
+    in showsPrec p $ map (\y -> map (\x -> l ! (x,y)) [x0 .. xn]) [y0 .. yn]
 
 sigmoid :: Double -> Double
 sigmoid t = 1 / (1 + exp (-t))
@@ -24,7 +40,10 @@ sigmoidDerivative fx = fx * (1 - fx)
 feedforward :: NNet -> [Double] -> [Double]
 feedforward = flip (foldl' feedlayer)
 
-feedlayer i = map (sigmoid . sum . zipWith (*) (1:i))
+feedlayer :: [Double] -> Layer -> [Double]
+feedlayer i (Layer l) = let
+  ((x0,y0),(xn,yn)) = bounds l
+  in map (\y -> sigmoid $ sum $ zipWith (\x i' -> (l ! (x,y)) * i') [x0 .. xn] (1:i)) [y0 .. yn]
 
 {-
 What was causing the bug
@@ -48,7 +67,8 @@ backprop rate i t n = backpropSome rate i (map Just t) n
 
 backpropSome :: Double -> [Double] -> [Maybe Double] -> NNet -> NNet
 backpropSome rate i t n = fst $ backprop' i t n where
-  backprop' i t (l:n) = (nw:r,be) where
+  backprop' i t (l@(Layer la):n) = (nw:r,be) where
+    ab@((x0,y0),(xn,yn)) = bounds la
     -- hs: output of this layer
     hs = feedlayer i l
     -- r: the next layer updated
@@ -60,30 +80,30 @@ backpropSome rate i t n = fst $ backprop' i t n where
        ) hs t)
       x -> backprop' hs t n
     -- we: Error divided among weights
-    we = zipWith (\oe w ->
-      map (*oe) w
-     ) e l
+    we :: UArray (Int,Int) Double
+    we = array ab $ do
+      (oe,y) <- zip e [y0 .. yn]
+      x <- [x0 .. xn]
+      return ((x,y), oe * (la ! (x,y)))
+      
     -- nw: New weights for this layer
     -- wl: weights leading to current node
     -- h: this node's output
-    nw = zipWith3 (\wl d h -> let sdh = sigmoidDerivative h in
-      -- w: The current weight
-      -- d: The error assigned to the current node
-      -- x: The input to the current synapse
-      zipWith (\w x ->
-        w + rate * d * sdh * x
-       ) wl (1:i)
-     ) l e hs
+    nw = Layer $ array ab $ do
+      (y,d,h) <- zip3 [y0 .. yn] e hs
+      let sdh = sigmoidDerivative h
+      (x,i') <- zip [x0 .. xn] (1 : i)
+      return ((x,y), (la ! (x,y)) + rate * d * sdh * i')
     -- be: Errors to propagate back to earlier nodes
-    be = map sum $ transpose we
+    be = map (\x -> sum $ map (\y -> we ! (x,y)) [y0 .. yn]) [x0 .. xn]
 
 randomNNet :: RandomGen g => g -> [Int] -> NNet
 randomNNet _ [_] = []
 randomNNet gen (i:r@(n:_)) = let
-  frl g _ = mapAccumL (\g _ -> let
-    (a,g') = randomR (-0.05,0.05) g
-    in (g',a)) g $ replicate (i+1) ()
-  frg g = mapAccumL frl g $ replicate n ()
-  (gen',l1) = frg gen
-  in l1 : randomNNet gen' r
+  i' = i + 1
+  (gen',l1) = mapAccumL (const . uncurry (flip (,)) . randomR (-0.05, 0.05)) gen $
+    replicate ((i + 1) * n) ()
+  ar = ((0,0),(i, n - 1))
+  l1' = Layer $ array ar $ zip (range ar) l1
+  in l1' : randomNNet gen' r
 
